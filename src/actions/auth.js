30f989ff7f16bda975f0f5482135d7294c8e85e7
kkoon9 <rndrnjs2003@gmail.com>
2023-12-12 21:06:33 +0900

    fix: api/auth > api/users/me 로 수정
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import { setAlert } from './alert';
-import setAuthToken from '../utils/setAuthToken';
+import axios from "axios";
+import { setAlert } from "./alert";
+import setAuthToken from "../utils/setAuthToken";
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -13,93 +13,95 @@ import {
 } from "./types";
 
 // Load User
-export const loadUser = () => async dispatch => {
+export const loadUser = () => async (dispatch) => {
   if (localStorage.token) {
     setAuthToken(localStorage.token);
   }
 
   try {
-    const res = await axios.get('/api/auth');
+    const res = await axios.get("/api/users/me");
 
     dispatch({
       type: USER_LOADED,
-      payload: res.data
+      payload: res.data,
     });
   } catch (error) {
     dispatch({
-      type: AUTH_ERROR
+      type: AUTH_ERROR,
     });
-  }
-}
-
-// Register User
-export const register = ({ name, email, password }) => async dispatch => {
-  const config = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }
-
-  const body = JSON.stringify({ name, email, password });
-
-  try {
-    const res = await axios.post('/api/users', body, config);
-
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data
-    });
-    dispatch(loadUser());
-
-  } catch (error) {
-    const errors = error.response.data.errors;
-
-    if (errors) {
-      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
-    }
-    dispatch({
-      type: REGISTER_FAIL
-    })
-  }
-}
-
-// Login User
-export const login = ({ email, password }) => async dispatch => {
-  const config = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }
-
-  const body = JSON.stringify({ email, password });
-
-  try {
-    const res = await axios.post('/api/auth', body, config);
-
-    dispatch({
-      type: LOGIN_SUCCESS,
-      payload: res.data
-    });
-    dispatch(loadUser());
-
-  } catch (error) {
-    const errors = error.response.data.errors;
-
-    if (errors) {
-      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
-    }
-    dispatch({
-      type: LOGIN_FAIL
-    })
   }
 };
 
+// Register User
+export const register =
+  ({ name, email, password }) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const body = JSON.stringify({ name, email, password });
+
+    try {
+      const res = await axios.post("/api/users", body, config);
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
+      dispatch(loadUser());
+    } catch (error) {
+      const errors = error.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      }
+      dispatch({
+        type: REGISTER_FAIL,
+      });
+    }
+  };
+
+// Login User
+export const login =
+  ({ email, password }) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const body = JSON.stringify({ email, password });
+
+    try {
+      const res = await axios.post("/api/auth", body, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+      dispatch(loadUser());
+    } catch (error) {
+      const errors = error.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      }
+      dispatch({
+        type: LOGIN_FAIL,
+      });
+    }
+  };
+
 // Logout / Clear Profile
-export const logout = () => dispatch => {
+export const logout = () => (dispatch) => {
   dispatch({
-    type: LOGOUT
+    type: LOGOUT,
   });
   dispatch({
-    type: CLEAR_PROFILE
+    type: CLEAR_PROFILE,
   });
-}
+};
